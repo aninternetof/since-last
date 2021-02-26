44ed644228d092d8d1e4ca62cfbe4e5a44d63d3d
Brady L. Hurlburt <brady.hurlburt@rovercode.com>
2021-02-25 21:17:49 -0500

    Add firebase with auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,69 @@
 import "./App.css";
 
+import React, { Component } from "react";
+import firebase from "firebase";
+
+import Button from "react-bootstrap/Button";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 
-function App() {
-  return (
-    <Container className="p-3">
-      <Jumbotron>
-        <h1 className="header">Welcome To React-Bootstrap</h1>
-      </Jumbotron>
-    </Container>
-  );
+class App extends Component {
+  authRef = null;
+
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    this.authRef = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({
+        user,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.authRef && this.authRef.off();
+  }
+
+  onSignInPress() {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
+  onSignOutPress() {
+    firebase.auth().signOut();
+  }
+
+  checkUser() {
+    if (!this.state.user) {
+      alert("You must be logged in");
+      return false;
+    }
+    return true;
+  }
+
+  render() {
+    let { user } = this.state;
+
+    return (
+      <Container className="p-3">
+        {user == null ? null : (
+          <Button onClick={this.onSignOutPress} variant="outline-dark">
+            Log out
+          </Button>
+        )}
+        <Jumbotron>
+          <h1 className="header">Welcome To React-Bootstrap</h1>
+        </Jumbotron>
+        {user == null ? (
+          <Button onClick={this.onSignInPress} variant="outline-dark">
+            Continue with your Google account
+          </Button>
+        ) : null}
+      </Container>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase";
 
 // Importing the Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import registerServiceWorker from "./registerServiceWorker";
+
+const config = {
+  apiKey: "AIzaSyBYXe4zNizE7cHJMEcyfxAof40YkZ-Bjhk",
+  authDomain: "sincelast-56533.firebaseapp.com",
+  projectId: "sincelast-56533",
+  storageBucket: "sincelast-56533.appspot.com",
+  messagingSenderId: "779277243443",
+  appId: "1:779277243443:web:27f40f8b69a339884fbf46",
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
